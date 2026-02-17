4565e74e1bc660ba443b53ebf8147c71070a15f5
gorathus6-sketch <gorathus6@gmail.com>
2026-02-17 12:41:47 -0500

    Update uberbossres2.cpp

--- uberbossres2.cpp
+++ uberbossres2.cpp
@@ -4,7 +4,7 @@
 void printIntroduction(int Difficulty)
 {
     // Print Welcome messages to the terminal
-    std::cout << "\n\n You are uberb0ss h4x0r 2.0. Your mission: to hack into an untheticl corporation;"
+    std::cout << "\n\n You are uberb0ss h4x0r 2.0. Your mission: to hack into an unthethical corporation;"
     std::cout << "\n\n Your uberb0ss h4x1ng level is " << Difficulty;
     std::cout << "\n\n Welcome back from retirement! Nead to break the code, yo...\n\n"; 
 }
