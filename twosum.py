bc6d442f82cb775b3d7e8a8564740fd4382c3452
gorathus6-sketch <gorathus6@gmail.com>
2026-02-19 21:57:03 -0500

    two sum in python
    two sum in python

---
+++ twosum.py
@@ -0,0 +1,9 @@
+def two_sum(nums, target):
+    prev_map = {}  # val : index
+
+    for i, n in enumerate(nums):
+        diff = target -n
+        if diff in prev_map:
+            return [prev_map[diff], i]
+        prev_map[n] = i
+    return []
