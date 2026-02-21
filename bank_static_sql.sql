50a463e6fd30dc4ee15a8027c183bcfa49ba5a87
gorathus6-sketch <gorathus6@gmail.com>
2026-02-21 07:45:11 -0500

    Update bank_static_sql.sql

--- bank_static_sql.sql
+++ bank_static_sql.sql
@@ -1,4 +1,5 @@
 INSERT INTO BANK (
+    CLIENT_ID,
     BANK_NAME,
     ABA_NUMBER,
     ADDRESS,
@@ -7,13 +8,15 @@ INSERT INTO BANK (
     ZIP_CODE,
     PHONE_NUMBER
 ) VALUES (
+    '001',
     'BANK NAME HERE',
     '123456789',
     '1 MAIN STREET',
     'NEW YORK',
     'NY',
-    '10000'
+    '10000',
     '1005550001'
 );
+
 
 COMMIT;
