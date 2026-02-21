6646c0e2052a66cf14aafb40293825ff05dc781e
gorathus6-sketch <gorathus6@gmail.com>
2026-02-21 05:59:06 -0500

    Template for adding new banking clients in Oracle
    Template for adding new banking clients in Oracle

---
+++ bank_static_sql.sql
@@ -0,0 +1,19 @@
+INSERT INTO BANK (
+    BANK_NAME,
+    ABA_NUMBER,
+    ADDRESS,
+    CITY,
+    STATE,
+    ZIP_CODE,
+    PHONE_NUMBER
+) VALUES (
+    'BANK NAME HERE',
+    '123456789',
+    '1 MAIN STREET',
+    'NEW YORK',
+    'NY',
+    '10000'
+    '1005550001'
+);
+
+COMMIT;
