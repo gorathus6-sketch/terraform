2918727ab0e283661a3c4b54e08ec9650d941ccd
gorathus6-sketch <gorathus6@gmail.com>
2026-02-22 21:09:21 -0500

    Bash script for checking disk space TH75
    Bash script for checking disk space TH75

---
+++ diskcheck.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+THRESHOLD=75
+FILESYSTEM='/'
+
+# extract the numeric percentage (eg n from n%)
+USAGE=$(df -h "FILESYSTEM" | awk 'NR==2 {gsub("%","",$5); print $5}')
+
+echo "Disk usage on $FILESYSTEM is ${USAGE}% (threshold: ${THRESHOLD}%)"
+
+if [ "USAGE" -gt "THRESHOLD" ]; then
+    echo "ERROR: Disk usage exceeds acceptable threshold."
+    exit 1
+else
+    echo "Disk usage within acceptable limits."
+    exit 0
+fi
