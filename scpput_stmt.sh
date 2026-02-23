68745e3af5b38aa582da5ce3ccec23ca7f3beecb
gorathus6-sketch <gorathus6@gmail.com>
2026-02-23 08:36:36 -0500

    Update scpput_stmt.sh

--- scpput_stmt.sh
+++ scpput_stmt.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
-# def variables (windows)
-CURRENT_DATE=$(date +%Y%m%d)
-REMOTE_USER=orsypftp
-REMOTE_HOST=192.168.103.44
-REMOTE_PATH='C:\inetpub\wwwroot\ftp\stmt_reports\'
-FILE_NAME=statement_${CURRENT_DATE}.txt
-LOCAL_DEST=/p4848/spool/
+# get current date
+CURRENTDATE=$(date +%Y%m%d)
 
-scp -i ~/.ssh/orsypftp.pem ${LOCAL_DEST}/${FILE_NAME} ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH} 
+# define the specific filename
+STMT_FILE=statement_$CURRENTDATE.txt
+
+# source and target dirs
+SPOOL_DIR=/p4848/spool
+SFTP_STMT_DIR=/mnt/c/inetpub/wwwroot/sftp/stmt_reports
+
+# full path
+FULL_SOURCE=$SPOOL_DIR/$STMT_FILE
+
+# perform the copy with a check
+if [ -f $FULL_SOURCE ]
+then
+    scp $FULL_SOURCE $SFTP_STMT_DIR
+    echo Success: $STMT_FILE has been sent to $SFTP_STMT_DIR
+else
+    echo Error: $STMT_FILE was not found in $SPOOL_DIR
+    exit 1
+fi 
