2cb5f5b6f0e8204db47a68c172b717c7f80c7f5b
gorathus6-sketch <gorathus6@gmail.com>
2026-03-06 22:02:55 -0500

    variables for diagnosticSettings

--- terraform/modules/diagnostic-settings/variables.tf
+++ terraform/modules/diagnostic-settings/variables.tf
@@ -1,3 +1,5 @@
-variable "name" { type = string }
-variable "location" { type = string }
-variable "resource_group_name" { type = string }
+variable "resource_name" { type = string }
+variable "target_resource_id" { type = string }
+variable "workspace_id" { type = string }
+variable "log_categories" { type = list(string) }
+variable "metric_categories" { type = list(string) }
