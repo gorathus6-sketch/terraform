17a97a643fc35e4d094ee90a2d13c8903f630dab
gorathus6-sketch <gorathus6@gmail.com>
2026-03-06 21:31:11 -0500

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,14 +1,16 @@
 terraform {
     required_version = ">= 1.6.0"
 
-    required_providers {
-        azure = {
-            source  = "hashicorp/azurerm"
-            version = "~> 4.0"
-        }
+    backend "azurerm" {
+        resource_group_name  = "tfstate-rg-dev"
+        storage_account_name = "tfstatestorageempathdev"
+        container_name       = "tfstate"
+        key                  = "dev.terrform.tfstate"
     }
 }
 
 provider "azurerm" {
-  features {}
+    features {}
+    tenant_id       = var.tenant_id
+    subscription_id = var.subscription_id
 }
