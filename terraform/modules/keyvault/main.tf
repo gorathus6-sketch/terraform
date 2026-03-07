f5f0a76771be4f70b6601eb18f6e14de3f4754e7
gorathus6-sketch <gorathus6@gmail.com>
2026-03-06 21:10:53 -0500

    main terraform for keyvault

---
+++ terraform/modules/keyvault/main.tf
@@ -0,0 +1,16 @@
+resource "azurerm_key_vault" "this" {
+    name                       = var.name
+    location                   = var.location
+    resource_group_name        = var.resource_group_name
+    tenant_id                  = var.tenant_id
+    sku_name                   = "standard"
+    purge_protection_enabled   = true
+    soft_delete_retention_days = 30
+    enable_rbac_authorization  = true
+
+    network_acls {
+        default_action             = "Deny"
+        bypass                     = "AzureServices"
+        virtual_network_subnet_ids = var.subnet_ids
+    }
+}
