279bfd799b5a3298bc1b00c67032ed312d38dac1
T Dawg <gorathus6@gmail.com>
2026-03-12 22:25:15 -0400

    SG main.tf

---
+++ aws_terraform/securitygroup/main.tf
@@ -0,0 +1,40 @@
+resource "aws_security_group" "this" {
+  name        = var.name
+  description = var.description
+  vpc_id      = var.vpc_id
+  tags        = var.tags
+}
+
+resource "aws_security_group_rule" "ingress" {
+  for each = var.ingress_rules
+
+  type              = "ingress"
+  security_group_id = aws_security_group.this.id
+
+  from_port   = each.value.from_port
+  to_port     = each.value.to_port
+  protocol    = each.value.protocol
+
+  cidr_block               = each.value.cidr_blocks
+  ipv6_cidr_blocks         = each.value.ipv6_cidr_blocks
+  security_groups          = each.value.source_security_groups
+  self                     = each.value.self_reference
+  description              = each.value.description
+}
+
+resource "aws_security_group_rule" "egress" {
+  for_each = var.egress_rules
+
+  type              = "egress"
+  security_group_id = aws_security_group.this.id
+
+  from_port  = each.value.from_port
+  to_port    = each.value.to_port
+  protocol   = each.value.protocol
+
+  cidr_blocks             = each.value.cidr_blocks
+  ipv6_cidr_blocks        = each.value.ipv6_cidr_blocks
+  security_groups         = each.value.destination_security_groups
+  self                    = each.value.self_reference
+  description             = each.value.description
+}
