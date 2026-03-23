805aacc7e6b3f4f545b7cba099c34bfb8f23d83d
T Dawg <gorathus6@gmail.com>
2026-03-23 12:35:04 -0400

    uberboss2.py
    Simple text based game coded in python

--- python/uberboss2.py
+++ python/uberboss2.py
@@ -1,14 +1,14 @@
 import random
 
 def print_introduction(difficulty):
-    print("\nYou are uberb0ss h4x0r 2.0. Your missions: hack into an unethical corporation.")
+    print("\n You are uberb0ss h4x0r 2.0. Your mission: hack into an unethical corporation.")
     print(f"Your uberb0ss h4x1ng level is {difficulty}.")
-    print("Welcome back from retirement! Time to break the code ... \n")
+    print("Welcome back from retirement! Time to break the code, yo. \n")
 
 def play_game(difficulty):
     print_introduction(difficulty)
 
-    # Generate 5 random numbers based on difficulty
+    # generate 5 random numbers based on difficulty
 
     code = [random.randint(difficulty, difficulty * 2) for _ in range(5)]
     code_sum = sum(code)
@@ -20,12 +20,12 @@ def play_game(difficulty):
         code_product *= n
 
     print("+ There are 5 numbers in the code")
-    print(f"+ The codes add up to: {code_sum}")
-    print(f"+ The codes multiply to give: {code_product}")
+    print(f"+ the codes add up to: {code_sum}")
+    print(f"+ the codes multiple to give: {code_product}")
 
     # player guess
 
-    guess_input = input("\nEnter your 5 guesses seperated by spaces: ")
+    guess_input = input("\n Enter your 5 guesses separated by spaces: ")
     try:
         guesses = list(map(int, guess_input.split()))
         if len(guesses) != 5:
@@ -34,7 +34,7 @@ def play_game(difficulty):
     except ValueError:
         print("Invalid input. Enter numbers only.")
         return False
-
+        
     guess_sum = sum(guesses)
 
     guess_product = 1
@@ -44,12 +44,13 @@ def play_game(difficulty):
     # check win condition
 
     if guess_sum == code_sum and guess_product == code_product:
-        print("\n*** You did it, uberb0ss h4x0r! Keep on h4x1ng! ***")
+
+        print("\n *** You did it, uberboss h4x0r! Keep on h4x1ng! ***")
         return True
     else:
-        print("\n*** WTF?! Your h4x1ng sucks, n00b! Try again! ***")
+        print("\n *** WTF?! Your h4x1ng fvck1ng sucks, n00b! Try again! ***")
         return False
-
+    
 def main():
     max_difficulty = 6
     level = 1
@@ -58,8 +59,7 @@ def main():
         if play_game(level):
             level += 1
 
-    print("\n*** EXEMPLARY WORK, UBERB0SS H4X0R! YOU NEVER FAIL! ***")
+    print("\n *** EXEMPLARY WORK, UBERB0SS H4X0R! YOU HEVER FAIL TO H4X THEM @***$ RAW! ***")
 
 if __name__ == "__main__":
     main()
-
