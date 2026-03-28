035985d4ae01cee3d3a86162fd5f691e5549dfe4
T Dawg <gorathus6@gmail.com>
2026-03-27 21:31:32 -0400

    Update main.tf with IDS

--- terraform/modules/firewall/main.tf
+++ terraform/modules/firewall/main.tf
@@ -2,4 +2,9 @@ resource "azurerm_firewall_policy" "this" {
     name                = var.name
     resource_group_name = var.resource_group_name
     location            = var.location
+    sku                 = "Premium" # for IDS
+
+    intrusion_detection {
+        mode = "Deny"
+    }
 }
