60397ad80d6b4cf226e7e0dedc928850a08fd33c
T Dawg <gorathus6@gmail.com>
2026-03-27 21:57:07 -0400

    Update variables.tf to include tenant_id and sub_id vars

--- terraform/modules/rbac/variables.tf
+++ terraform/modules/rbac/variables.tf
@@ -1,3 +1,5 @@
 variable "scope" { type = string }
 variable "role_definition_name" { type = string }
 variable "principal_id" { type = string }
+variable "tenant_id_dev" { type = string }
+variable "subscription_id_dev" { type = string }
